52843a8087f3e6d2a3ada8457cd6344acff39748
Niranjan Khanal <niranjankhanal44@gmail.com>
2026-09-04 21:37:40 +0545

    update code
    Commented out code for factorial, multiplication table, and prime check functionalities.

--- Main.java
+++ Main.java
@@ -1,3 +1,4 @@
+code for factorial
 // import java.util.*;
 // public class Main{
 //     public static void fact(int x){
@@ -18,7 +19,7 @@
 //     }
 // }
 
-
+code for multiplication table
 // import java.util.*;
 // public class Main{
 //     public static void multitable(int x)
@@ -38,37 +39,37 @@
 //         multitable(num);
 //     }
 // }
-
-import java.util.*;
-public class Main{
-    public static void checkprime(int x)
-    {
-        int i,flag=0;
-        if(x<2)
-        {   
-            System.out.println("not prime");
-        }
-        for(i=2;i<=x;i++)
-        {
-            if(x%i==0)
-            {
-                flag=1;
-                break;
-            }
-        }
-        if(flag==0)
-        {
-            System.out.println("prime");
-        }
-        else
-        {
-            System.out.println("not prime");
-        }
-    }
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("enter number ");
-        int num = sc.nextInt();
-        checkprime(num);
-    }
-}
+code for prime check
+// import java.util.*;
+// public class Main{
+//     public static void checkprime(int x)
+//     {
+//         int i,flag=0;
+//         if(x<2)
+//         {   
+//             System.out.println("not prime");
+//         }
+//         for(i=2;i<=x;i++)
+//         {
+//             if(x%i==0)
+//             {
+//                 flag=1;
+//                 break;
+//             }
+//         }
+//         if(flag==0)
+//         {
+//             System.out.println("prime");
+//         }
+//         else
+//         {
+//             System.out.println("not prime");
+//         }
+//     }
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         System.out.println("enter number ");
+//         int num = sc.nextInt();
+//         checkprime(num);
+//     }
+// }
