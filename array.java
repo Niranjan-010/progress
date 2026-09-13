9ad46c1beffad03ff698c5cc80ac15d123dcaa9f
Niranjan <niranjankhanal44@gmail.com>
2026-09-13 16:02:55 +0545

    add codes

--- array.java
+++ array.java
@@ -176,3 +176,121 @@
 //         }
 //     }
 // }
+
+// import java.util.*;
+// public class array{
+//     public static void main(String [] args)
+//     {
+//         Scanner sc = new Scanner(System.in);
+//         System.out.println("enter value of n");
+//         int n = sc.nextInt();
+//         int [] num = new int[n];
+//         System.out.println("enter the elements of array");
+//         for(int i=0;i<n;i++)
+//         {
+//             num[i] = sc.nextInt();
+//         }
+//         System.out.println("enter the number you want to search in array");
+//          int x = sc.nextInt();
+//         for(int i=0;i<n;i++)
+//             {
+//                 if(num[i]==x)
+//                 {
+//                     System.out.println("Number found at index "+i);
+//                      break;
+//                 }
+//             }
+//              System.out.println("sorry not found ");
+//     }
+// // }
+// import java.util.*;
+// class array{
+//     public static void main(String [] args)
+//     {
+//         Scanner sc = new Scanner(System.in);
+//         System.out.println("enter value of n");
+//         int n = sc.nextInt();
+//         int [] num = new int[n];
+//         System.out.println("enter the elements of array");
+//         for(int i=0;i<n;i++)
+//         {
+//             num[i] = sc.nextInt();
+//         }
+//         int max = num[0];
+//         int min = num[0];
+//         for(int i=0;i<n;i++)
+//             {
+//                if(num[i] > max)
+//                {
+//                 max = num[i];
+//                }
+//                if(num[i] < min)
+//                 {
+//                 min = num[i];
+//                 }
+//     }
+//     System.out.println("maximum number in array is "+max);
+//     System.out.println("minimum number in array is "+min);
+//     }
+// }
+//  import java.util.*;
+// public class array{
+//     public static void main(String [] args)
+//     {
+//         Scanner sc = new Scanner(System.in);
+//         System.out.println("enter value of row");
+//         int r = sc.nextInt();
+//          System.out.println("enter value of column");
+//         int c = sc.nextInt();
+//         int [][] num = new int[r][c];
+//         System.out.println("enter the elements of array");
+//         for(int i=0;i<r;i++)
+//         {
+//             for(int j = 0; j<c;j++)
+//             {
+//             num[i][j] = sc.nextInt();
+//             }
+//         }
+//         System.out.println("enter the number you want to search in array");
+//          int x = sc.nextInt();
+//         for(int i=0;i<r;i++)
+//             {
+//                 for(int j = 0; j<c;j++)
+//                 {
+//                 if(num[i][j]==x)
+//                 {
+//                     System.out.println("Number found at index  ["+i+"]["+j+"]");
+//                      break;
+//                 }
+//             }
+//         }
+//              System.out.println("sorry not found ");
+//     }
+//  }
+import java.util.*;
+public class array {
+   public static void main(String args[]) 
+   {
+      Scanner sc = new Scanner(System.in);
+      int n = sc.nextInt();
+      int m = sc.nextInt();
+      int matrix[][] = new int[n][m];
+      for(int i=0; i<n; i++) 
+        {
+           for(int j=0; j<m; j++)
+             {
+               matrix[i][j] = sc.nextInt();
+           }
+      }
+      System.out.println("The transpose is : ");
+      //To print transpose
+      for(int j=0; j<m ;j++) 
+        {
+          for(int i=0; i<n; i++) 
+            {
+              System.out.print(matrix[i][j]+" ");
+          }
+          System.out.println();
+      }
+   }
+}
