85e6f280752c3fdbfff38da2de9845bb4c3ac542
Niranjan Khanal <niranjankhanal44@gmail.com>
2026-09-14 22:06:31 +0545

    Update strings.javadone

--- strings.java
+++ strings.java
@@ -122,3 +122,54 @@
 //         System.out.println(sb);
 //     }
 // }
+
+// // Reverse a String (using StringBuilder class)
+
+// // import java.util.*;
+
+
+// // public class Strings {
+// //    public static void main(String args[]) {
+// //      StringBuilder sb = new StringBuilder("HelloWorld");
+    
+// //      for(int i=0; i<sb.length()/2; i++) {
+// //        int front = i;
+// //        int back = sb.length() - i - 1;
+
+
+// //        char frontChar = sb.charAt(front);
+// //        char backChar = sb.charAt(back);
+
+
+// //        sb.setCharAt(front, backChar);
+// //        sb.setCharAt(back, frontChar);
+// //      }
+
+
+// //      System.out.println(sb);
+// //    }
+// // }
+
+// Append means to add something at the end.
+
+// import java.util.*;
+// public class Strings {
+//    public static void main(String args[]) {
+//      StringBuilder sb = new StringBuilder("Tony");
+//      sb.append(" Stark");
+//      System.out.println(sb); 
+//    }
+// }
+
+// Print Length of String
+
+
+// import java.util.*;
+// public class Strings {
+//    public static void main(String args[]) {
+//      StringBuilder sb = new StringBuilder("Tony");
+//      sb.append(" Stark");
+//      System.out.println(sb); 
+//      System.out.println(sb.length());
+//    }
+// }
