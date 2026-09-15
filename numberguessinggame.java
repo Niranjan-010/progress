39cb4421934a301ad3bc9c52f9a0279442003b66
Niranjan Khanal <niranjankhanal44@gmail.com>
2026-09-15 07:25:58 +0545

    add

---
+++ numberguessinggame.java
@@ -0,0 +1,50 @@
+import java.util.*;
+class numberguessinggame{
+public static void main(String [] args)
+{
+int num = 29;
+System.out.println("-----Welcome to number guessing game----");
+Scanner sc = new Scanner(System.in);
+while(true)
+{
+System.out.println("enter y/n to proceed ");
+String ch = sc.next();
+if(ch.equalsIgnoreCase("y"))
+{
+System.out.println("Let's goo... ");
+System.out.println("Reminder ! you have only 5 attempts");
+System.out.println("enter number to guess ");
+Boolean won = false;
+for(int i = 0; i<5;i++)
+{
+int n = sc.nextInt();
+if(n > num)
+{
+System.out.println("you're diverging form this");
+}
+else if(n< num)
+{
+System.out.println("you're converging now");
+}
+else 
+{
+System.out.println("congrat's , you did it !");
+won = true;
+}
+if(i==4&&!won)
+{
+    System.out.println("you've exhausted your attempts");
+    System.out.println("better luck next time");
+}
+}
+}
+else
+{
+System.out.println("Thank's for your choice ");
+System.out.println("Have a nice day");
+break;
+}
+}
+sc.close();
+}
+}
